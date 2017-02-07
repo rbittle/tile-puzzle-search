22382b3df1e1883dc231749a19304ffab5414737
Riley Bittle <rbittle34@gmail.com>
2017-02-06 19:36:26 -0600

    implemented queues, all functions use (cost, string) to represent states

--- search.py
+++ search.py
@@ -1,21 +1,43 @@
+#! /bin/python3
+
 import argparse
 import re
+from queue import Queue, PriorityQueue, LifoQueue
 
-'''
+# command line argument parsing
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-cost", action="store_true")
-parser.add_argument("search-type")
-parser.add_argument("input-file")
+parser.add_argument("search_type")
+parser.add_argument("input_file")
 
 args = parser.parse_args()
-'''
+
+
 
 class TileGame:
     def __init__(self, initial_state): 
-        self.tiles = initial_state
+        self.initial_state = initial_state
         self.visited = []
+
+        if args.search_type is "BFS":
+            self.q= Queue()
+        elif args.search_type is "DFS":
+            self.q= LifoQueue()
+        else:
+            self.q = PriorityQueue()
     
+    def search(self):
+        self.q.put((0, self.initial_state))
+        
+        #test
+        first = self.q.get()
+        print(next_states(first[1]))
+        #endtest
+
+    def available(self):
+        pass
+
 def test_valid(state):
     # checks if the string contains a series of w, b or x
     test = re.compile(r"^[wWbB]*[xX][wWbB]*$")
@@ -24,11 +46,19 @@ def test_valid(state):
 
 def swap_x(state, i):
     i_char = state[i]
+    x_pos = state.index('x')
+
+    if args.cost:
+        cost = abs(i - x_pos)
+    else:
+        cost = 1
+
     new_state = state.replace('x', i_char, 1)
     new_state = list(new_state) # array of chars
     new_state[i] = 'x'
     new_state = ''.join(new_state)
-    return new_state
+
+    return (cost, new_state)
 
 def next_states(state):
     # gets an array of possible states possible one move from the start state, including the cost
@@ -39,11 +69,15 @@ def next_states(state):
     for i, tile in enumerate(state_array):
         if tile is 'x':
             continue
-        # new_state is a tuple (state, cost)
+        # new_state is a tuple (cost, state)
         new_state = swap_x(state, i)
-        if new_state[0] not in self.visited:
-            next_states.append(new_state)
+        next_states.append(new_state)
 
     return next_states       
 
-print(swap_x("WWxB", 1))
+
+
+#TESTING#
+
+test = TileGame("WBxW")
+test.search()
