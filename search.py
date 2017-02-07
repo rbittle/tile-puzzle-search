8a0dd0ac2729f966696f117bbe5bc7093562592a
Riley Bittle <rbittle34@gmail.com>
2017-02-07 17:22:13 -0600

    Proper printing of solution path

--- search.py
+++ search.py
@@ -19,7 +19,7 @@ class TileGame:
     def __init__(self, initial_state): 
         self.initial_state = initial_state
         self.visited = []
-        self.opened = []
+        self.opened = {}
 
         if args.search_type.upper() == "BFS":
             self.q = Queue()
@@ -28,45 +28,50 @@ class TileGame:
         else: # UCS, GS, A-star
             self.q = PriorityQueue()
     
-    def available(self, state):
+    def available(self, state_tup):
         # passing sring to get possible states
-        possible_states = next_states(state)
+        possible_states = next_states(state_tup)
+        state = state_tup[1]
         # filter out already visited states
         valid_states = [state for state in possible_states if state[1] not in self.visited]
         return valid_states 
 
     def deconstruct_solution(self, solution_state):
-        parents = {}
-        for node in self.opened:
-            parents[node[1]] = next((parent for parent in self.opened if parent[1] == node[2]), "start")
-
         current = solution_state
         step = 0
-#TODO
-        while current != 'start':
-            # print("Step "+str(step)+":  move "+str(current[3])+" "+current[1])
-            print(current)
-            step += 1
-            current = parents[current[2]]
-        print(current)
+        path = LifoQueue()
 
+        path.put(current)
+        while self.opened[current[1]] != "start":
+            current = self.opened[current[1]]
+            path.put(current)
+
+        while not path.empty():
+            node = path.get()
+            if node[3] == "start":
+                print("Step "+str(step)+":  "+node[1])
+            else:
+                print("Step "+str(step)+":  move "+str(node[3])+" "+node[1])
+            step += 1
+        
     def search(self):
-        self.q.put((0, self.initial_state, "start", -1))
-        print("Start: "+self.initial_state)
+        self.q.put((0, self.initial_state, "start", "start"))
+        self.opened[self.initial_state] = "start"
+        # print("Start: "+self.initial_state)
         
         while not self.q.empty():
             current_state = self.q.get()
-            self.opened.append(current_state)
-            print("Visiting: " + current_state[1] + " from " + str(current_state[2]))
+            # print("Visiting: " + current_state[1] + " from " + str(current_state[2]))
             # add current state to visited states
             self.visited.append(current_state[1])
             # check if the state is the solution
             if test_solution(current_state[1]):
-                print("Solution found\n")
+                # print("Solution found\n")
                 self.deconstruct_solution(current_state)
                 break
 
-            for next_state in self.available(current_state[1]):
+            for next_state in self.available(current_state):
+                self.opened[next_state[1]] = current_state
                 self.q.put(next_state)
 
         # add available states to queue
@@ -99,7 +104,9 @@ def heuristic(state):
 
     return cost
 
-def swap_x(state, i):
+def swap_x(state_tup, i):
+    state = state_tup[1]
+
     i_char = state[i]
     x_pos = state.index('x')
 
@@ -120,10 +127,11 @@ def swap_x(state, i):
         cost += heuristic(new_state)
 
     # new state is a tuple (cost, state, prev_state)
-    return (cost, new_state, state, i)
+    return (cost + state_tup[0], new_state, state, i)
 
-def next_states(state):
+def next_states(state_tup):
     # gets an array of possible states possible one move from the start state, including the cost
+    state = state_tup[1]
     state_array = list(state)
     
     next_states = []
@@ -132,17 +140,18 @@ def next_states(state):
         if tile is 'x':
             continue
         # new_state is a tuple (cost, state, prev_state)
-        new_state = swap_x(state, i)
+        new_state = swap_x(state_tup, i)
         next_states.append(new_state)
 
     return next_states       
 
 
 if __name__ == "__main__":
-    puzzle = "WxB"
+    f = open(args.input_file, 'r')
+    puzzle = f.read().strip()
 
     if not test_valid(puzzle):
-        print("Starting state not valid")
+        print("Puzzle not valid")
     else:
         game = TileGame(puzzle)
         game.search()
